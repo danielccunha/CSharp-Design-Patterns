5718cf1e4db8d0aed1b3ccf9f079d06a527f91c8
danielccunha <danielcunha54@gmail.com>
2019-04-25 22:22:19 -0300

    Dependency Inversion principle

--- Patterns/Patterns/Program.cs
+++ Patterns/Patterns/Program.cs
@@ -5,15 +5,9 @@ namespace Patterns
 {
     class Program
     {
-        public static int Area(Rectangle rectangle) => rectangle.Height * rectangle.Width;
-
         static void Main()
         {
-            var rectangle = new Rectangle(10, 20);
-            Console.WriteLine($"{rectangle} has area {Area(rectangle)}");
-
-            var square = new Square(20);
-            Console.WriteLine($"{square} has area {Area(square)}");
+            Research.Start();
         }
     }
 }
---
+++ Patterns/Patterns/SOLID/DependencyInversion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Patterns.SOLID
+{
+    public enum Relationship
+    {
+        Parent,
+        Child,
+        Sibling
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public interface IRelationshipBrowser
+    {
+        IEnumerable<Person> FindAllChildrenOf(string name);
+    }
+
+    // Low-level
+    public class Relationships : IRelationshipBrowser
+    {
+        private readonly List<(Person, Relationship, Person)> _relations
+            = new List<(Person, Relationship, Person)>();
+
+        public void AddParentAndChild(Person parent, Person child)
+        {
+            _relations.Add((parent, Relationship.Parent, child));
+            _relations.Add((child, Relationship.Child, parent));
+        }
+
+        public IEnumerable<Person> FindAllChildrenOf(string name)
+        {
+            return _relations
+                .Where(r => r.Item1.Name == name && r.Item2 == Relationship.Parent)
+                .Select(r => r.Item3);
+        }
+    }
+
+    public class Research
+    {
+        public Research(IRelationshipBrowser browser)
+        {
+            foreach (var p in browser.FindAllChildrenOf("John"))
+                Console.WriteLine($"John has a child called {p.Name}");
+        }
+
+        public static void Start()
+        {
+            var parent = new Person("John");
+            var childOne = new Person("Chris");
+            var childTwo = new Person("Mary");
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, childOne);
+            relationships.AddParentAndChild(parent, childTwo);
+
+            new Research(relationships);
+        }
+    }
+}
